282210ccc3737d91f0339dc65d7236553f1a0d2a
Irshadwev <irshadwebdev9@gmail.com>
2026-05-13 16:13:28 +0500

    Solve Problems

---
+++ List/2.OnePlus/solution.py
@@ -0,0 +1,11 @@
+digit = [9] 
+digits_in_str = ''
+result = []
+for i in range(-1, -len(digit)-1, -1):
+    b = str(digit[i])
+    digits_in_str = b + digits_in_str
+digits_in_int = int(digits_in_str) + 1
+for i in str(digits_in_int):
+    result.append(int(i))
+
+print(result)
