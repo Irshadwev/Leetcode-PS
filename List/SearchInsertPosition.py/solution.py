d7f55a82a29d0d9807251d7a81e8f1df54cbce0e
Irshadwev <irshadwebdev9@gmail.com>
2026-05-23 21:02:16 +0500

    Add New PS

---
+++ List/SearchInsertPosition.py/solution.py
@@ -0,0 +1,30 @@
+'''
+35. Search Insert Position
+
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+'''
+
+nums = [1,3,5,6]
+target = 7
+result = 0
+if (target not in nums):
+        nums.append(target)
+        nums.sort()
+        print(nums.index(target))
+if (target in nums):
+        print(nums.index(target))
