50f8c81a2802e2fbbc061af0f50604fa46c7fc40
Irshadwev <irshadwebdev9@gmail.com>
2026-05-31 20:40:35 +0500

    Add ValidPalindrome PS

---
+++ String/ValidPalindrome/solution.py
@@ -0,0 +1,53 @@
+'''
+125. Valid Palindrome
+
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+Constraints:
+
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters.
+'''
+
+s = "A man, a plan, a canal: Panama"
+lowercaseS = s.lower()
+
+alpha = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+    'u', 'v', 'w', 'x', 'y', 'z'
+]
+
+s1 = ""
+for i in lowercaseS:
+    if(i in alpha):
+        s1 += i
+
+s2 = ""
+for i in range(-1, -1-len(s1), -1):
+    s2 += s1[i]
+
+if (s1 == s2):
+    print("True")
+else:
+    print("False")
